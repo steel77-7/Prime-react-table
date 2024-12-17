893e0d91bb3a24e0e8afb22f5410c0dc7e395b8a
steel77-7 <utkarshkaundal1970@gmail.com>
2024-12-17 16:53:35 +0530

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import TableComponent from "./components/TableComponent";
-
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import "primereact/resources/primereact.min.css"
+import "primereact/resources/primereact.min.css";
 
+import { PrimeReactProvider } from "primereact/api";
 
-import { PrimeReactProvider } from 'primereact/api';
-        
 function App() {
-  const [count, setCount] = useState(0);
- 
-  
   return (
     <>
       <PrimeReactProvider value={{ unstyled: false }}>
